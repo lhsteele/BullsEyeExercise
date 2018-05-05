fbbca28060d973c9670503ae7679243ac9871c88
Lisa Steele <lisahsteele@gmail.com>
2018-05-04 21:32:20 -0700

    Added all UI elements. Slider is set up to record value for comparison. Have also included the random number generating method.

--- BullsEyeExercise/ViewController.swift
+++ BullsEyeExercise/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        currentValue = lroundf(slider.value)
+        
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,13 +28,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
+        //The \n tells xcode to insert a special "new line" character at this point, which will
+        //break up the text into two lines so the message is easier to read.
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 }
 
