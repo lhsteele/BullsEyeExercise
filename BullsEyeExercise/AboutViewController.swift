91e4bebb1efc950e992f670675be71ef63f10f77
Lisa Steele <lisahsteele@gmail.com>
2018-05-15 21:24:54 -0700

    Added an "About" page, and connected the information button to present new View Controller. Also connected the Close button so player can return to the game view.

---
+++ BullsEyeExercise/AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.swift
+//  BullsEyeExercise
+//
+//  Created by Lisa Steele on 5/15/18.
+//  Copyright © 2018 lisahsteele. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
